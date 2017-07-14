152cbf40bed54716f0866dbe254405eba80a4ef7
acoshift <acoshift@gmail.com>
2017-07-14 20:23:32 +0700

    update

--- cmd/gonews/main.go
+++ cmd/gonews/main.go
@@ -3,22 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/acoshift/gonews/pkg/app"
 	"github.com/acoshift/gonews/pkg/model"
 )
 
 const (
-	port     = ":8080"
-	mongoURL = "mongodb://127.0.0.1:27017"
+	// mongoURL = "mongodb://127.0.0.1:27017"
+	mongoURL = "mongodb://gonews:9NAJHQXu4M9CvKqD@ds147872.mlab.com:47872/gonews"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	mux := http.NewServeMux()
 	app.Mount(mux)
 	err := model.Init(mongoURL)
 	if err != nil {
 		log.Fatalf("can not init model; %v", err)
 	}
-	http.ListenAndServe(port, mux)
+	http.ListenAndServe(":"+port, mux)
 }
